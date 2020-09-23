c08a7b3713bbcda5b1942afd642fd8e1638e5512
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-09-24 00:14:38 +0900

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class TodoModel(models.Model):
     #Charfield
+    #migrationのカラムの設定
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    #テーブルのカラム名ような名前を作成
+    def __str__(self):
+        return self.title
     
