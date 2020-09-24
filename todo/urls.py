ac7de34477b8bfe41be10442a317730e17a5c1fb
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-09-25 01:21:08 +0900

    add create & delete

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-from .views import TodoList, TodoDetail
+from .views import TodoList, TodoDetail, TodoCreate, TodoDelete
 
 urlpatterns = [
     #TodoListは、クラス名にas_view()
-    path('list/', TodoList.as_view()),
-    path('detail/<int:pk>', TodoDetail.as_view()),
+    path('list/', TodoList.as_view(), name='list'),
+    path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
+    path('create/', TodoCreate.as_view(), name='create'),
+    path('delete/<int:pk>',TodoDelete.as_view(), name='delete'),
+
 ]
+
