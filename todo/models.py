67843429ca55e167b829ff9376adf191efac6166
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-09-25 01:20:45 +0900

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+#''
+PRIORITY = (('danger', 'high'), ('info', 'normal'), ('success', 'low'))
 class TodoModel(models.Model):
     #Charfield
     #migrationのカラムの設定
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length = 50,
+        choices = PRIORITY
+    )
+    duedate = models.DateField()
+
     #テーブルのカラム名ような名前を作成
     def __str__(self):
         return self.title
-    
