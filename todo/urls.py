7907b4a61df1b5bde187bc0e9b5498c61412b72b
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-09-24 23:15:20 +0900

    add TodoDetail

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import TodoList
+from .views import TodoList, TodoDetail
 
 urlpatterns = [
     #TodoListは、クラス名にas_view()
     path('list/', TodoList.as_view()),
+    path('detail/<int:pk>', TodoDetail.as_view()),
 ]
